f0356193fe66c83d6cec1f0c3c4ca0a2e464f060
ngyan74 <ngyanchi11@gmail.com>
2026-02-23 22:23:43 -0500

    Move files initial setup

---
+++ src/data/projects.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    type: "image",
+    title: "Video Game Trends Analyzer",
+    description: "Full-stack data visualizer application using Video game sales and Google Trends datasets",
+    tech: ["React", "Express.js", "MongoDB"],
+    image: "/assets/project1.png",
+    link: "https://gitlab.com/dawson-cst-cohort-2026/520/section2/teams/TeamL-23-JenSungeunYanChi/520-project-huang-ng-kim"
+  },
+  {
+    type: "image",
+    title: "Barber Shop Booking Website",
+    description: "Full-stack barber shop application for both clients and professionals to view appointments",
+    tech: ["Python", "Flask", "JavaScript"],
+    image: "/assets/project2.png",
+    link: "https://gitlab.com/sever-side-cult-2025/prj-grp2-ng/-/tree/Deployment?ref_type=heads"
+  },
+  {
+    type: "image",
+    title: "Metro Trip Planner",
+    description: "Simple React app that displays metro routes and station info based on user input",
+    tech: ["React", "Express", "Vite"],
+    image: "/assets/project3.png",
+    link: "https://gitlab.com/dawson-cst-cohort-2026/520/section2/YanChiNg/assignment2/-/tree/A2-resub?ref_type=heads"
+  },
+  {
+    type: "image",
+    title: "AscendBook: Book Reading App",
+    description: "Android application that allows users to read, search, download, or listen to books via TTS",
+    tech: ["Kotlin", "Jetpack Compose", "SQLite/Room"],
+    image: "/assets/project4.png",
+    link: "https://gitlab.com/dawson-cst-cohort-2026/511/section2/David-Janelle-Yan-Chi/mobile-dev-project"
+  },
+  {
+    type: "video",
+    title: "MonoGame Fireworks Simulator",
+    description: "A MonoGame-based application that simulates fireworks physics and particles, featuring custom firework displays",
+    tech: ["C#", "MonoGame"],
+    video: "/assets/fireworks.mp4",
+    link: "https://gitlab.com/dawson-cst-cohort-2026/510/section2/ng/fireworks"
+  }
+];
