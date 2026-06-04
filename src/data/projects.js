b2246ba3d064a434789c67a24308735ba006eaaf
ngyan74 <ngyanchi11@gmail.com>
2026-06-04 18:05:10 -0400

    Add Stocked, 3D Asset Downloader, Reachout AI to projects page

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,22 +1,23 @@
 export default [
   {
     type: "image",
-    title: "Video Game Trends Analyzer",
-    short_description: "Full-stack data visualizer application using Video game sales and Google Trends datasets. Teammates: Sungeun Kim and Jennifer Huang",
-    description: "This web app displays data from two datasets: video game sales and Google trends data. We transform and load the data into Mongo, then serve it through a Node/Express API and visualize it with React through data charts and forms. Our goal with this project is to explore the connection between pop culture search trends and video game sales performances for a given year from 2001 - 2016 and look for correlations. I got much more familiar with React and using chart libraries through this project, and it inspired me to create more websites like this portfolio site to practice my front-end skills and share my work!",
-    tech: ["React", "Express.js", "MongoDB"],
-    image: "assets/project1.png",
-    link: "https://github.com/yan-74/games-trends-analyzer",
-    deployment: "https://five20-project-huang-ng-kim.onrender.com/"
-  },/*
+    title: "Stocked - Trading Platform",
+    short_description: "Full-stack React/Express practice trading platform using virtual currency and educational courses to teach trading fundamentals. Teammates: Artem Brandt, Riad Maaji, Antoine Labbe",
+    description: "This is a web-based trading simulation platform built for traders who want to learn skills without financial risk. Users can create accounts, access educational courses from partners, trade with virtual currency, and track their portfolio over time. The platform features real-time stock data, interactive lessons with progress tracking & quizzes, and a Gemini-powered trading assistant. I worked mostly on the courses feature of the project, developing both frontend and backend components. It was a great opportunity for me to apply my full-stack development skills and learn more about financial markets and trading concepts, and I'm really proud with how the final product turned out.",
+    tech: ["React", "Express.js", "Supabase", "Microsoft Azure", "Gemini"],
+    image: "assets/projects/stocked/stocked-1.png",
+    link: "https://github.com/yan-74/stocked-project",
+    deployment: "https://stocked-production-d6hfhqduhmfmcbhg.eastus-01.azurewebsites.net/"
+  },
   {
     type: "image",
-    title: "Barber Shop Booking Website",
-    description: "Full-stack barber shop application for both clients and professionals to view appointments",
-    tech: ["Python", "Flask", "JavaScript"],
-    image: "assets/project2.png",
-    link: "https://github.com/yan-74/barber-shop-app/tree/Deployment"
-  },*/
+    title: "Dawson 3D Asset Downloader",
+    short_description: "Desktop Application for downloading and filtering 3D assets from Autodesk Flow/Shotgrid in an organised manner. Teammates: Shuyuan Yin",
+    description: "For my internship project, I worked with stakeholders from the Dawson 3D Animation department to develop a desktop application that allows professors to download and filter student projects and files in bulk from the Autodesk Flow API. Users can do bulk zip downloads for projects/files, or download them individually in sequence, as well as filter and sort them by various fields. This was a really rewarding experience, as I got to work with industry professionals and practice professional skills like communication, coordinating meetings, and understanding user requirements. It was also a great opportunity to learn about Electron and how to build desktop applications, and it was nice to build something that would directly help the people who use it.",
+    tech: ["React", "Express.js", "Electron", "Autodesk Flow/Shotgrid"],
+    image: "assets/projects/3d-downloader/3d-2.png",
+    link: "https://github.com/yan-74/dawson-3d-asset-downloader"
+  },
   {
     type: "video",
     title: "MonoGame Fireworks Simulator",
@@ -27,20 +28,21 @@ export default [
     link: "https://github.com/yan-74/fireworks-simulator"
   },
   {
+    type: "video",
+    title: "Reachout AI",
+    short_description: "Express.js server that bridges Twilio and Gemini, allowing phones to utilise AI capabilities through SMS messages or calls. Teammates: Talon Dunbar, Jaden Ho, Artem Brandt",
+    description: "Reachout AI bridges the gap between the 2.7 billion people without smartphones and modern AI technology. We set up a Twilio phone number to receive SMS messages and calls, then used the Twilio API to forward those inputs to our Express server. The server processes the inputs and sends them to Gemini for AI responses, which are then sent back to the user via Twilio. This project was our submission for DawsHacks, and I learned a lot working with integrating SMS messages, voice calls, and the Gemini API--skills I'm not familiar with--into an application under a short timeframe. I'm proud of the features we were able to implement, and we ended up winning the hackathon, which was an amazing experience. ",
+    tech: ["Express.js", "Twilio", "Gemini", "SQLite"],
+    video: "https://www.youtube.com/embed/StS6GObsJNQ",
+    link: "https://github.com/talonlikeaclaw/dawshacks-2026"
+  },
+  {
     type: "image",
     title: "AscendBook: Book Reading App",
     short_description: "Android application that allows users to read, search, download, or listen to books via TTS. Teammates: David Cabanas Chiong and Janelle Annes Reyes",
     description: "This is an Android application built with Jetpack Compose that allows users to read, search, download, or listen to books via text-to-speech. We fetched book data from Project Gutenberg and implemented features like a search bar, book details page, and TTS functionality. This project was a great opportunity for me to learn about Android development and work with new tools and libraries, however coding with Kotlin was quite frustrating (especially the syntax). It was something unique to experiment with, and though I struggled a lot with the coding process, I'm glad I stepped out of my comfort zone and was able to create a functional app in the end.",
     tech: ["Kotlin", "Jetpack Compose", "SQLite/Room"],
-    image: "assets/project4.png",
+    image: "assets/projects/ascendbook.png",
     link: "https://github.com/yan-74/ascend-book-app"
   },
-  /*{
-    type: "image",
-    title: "Metro Trip Planner",
-    description: "Simple React app that displays metro routes and station info based on user input",
-    tech: ["React", "Express", "Vite"],
-    image: "assets/project3.png",
-    link: "https://gitlab.com/dawson-cst-cohort-2026/520/section2/YanChiNg/assignment2/-/tree/A2-resub?ref_type=heads"
-  },*/
 ];
